37792b93240619af9b10a46187b82ef1a02630e1
Sean <sean.t.irby@gmail.com>
2014-03-20 01:50:58 -0700

    Add keyboard text

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 plotter = (function(){
-  var plotWidth = 800,
-      plotHeight = 800,
+  var plotWidth = 1500,
+      plotHeight = 1500,
       paddingX = 20,
       paddingY = 20,
       newData = [],
@@ -15,8 +15,8 @@ plotter = (function(){
                       });
       return {
         init: function(keyboard){
-          xScale = d3.scale.linear().domain([0, 1]).range([0, 30]);
-          yScale = d3.scale.linear().domain([0, 1]).range([0, 30]);
+          xScale = d3.scale.linear().domain([0, 1]).range([0, 65]);
+          yScale = d3.scale.linear().domain([0, 1]).range([0, 65]);
 
           var rows = canvas.selectAll("g.row-key")
             .data(keyboard)
@@ -32,6 +32,16 @@ plotter = (function(){
             .attr("height", function(d){return yScale(d.height)})
             .attr("rx", function(d){return xScale(0.1)})
             .attr("ry", function(d){return xScale(0.1)})
+
+
+          rows.selectAll("text.key-name")
+            .data(function(d){return d})
+            .enter()
+            .append("svg:text")
+            .text(function(d){return d.name.toUpperCase()})
+            .attr({fill: 'white', "text-anchor": "middle", "font-size": function(d){return xScale(0.2)}})
+            .attr("x", function(d){return xScale(d.x + d.width/2)})
+            .attr("y", function(d){return yScale(d.y + d.height/2)})
         }
       }
 })();
