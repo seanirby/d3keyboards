e1bf09ec00ab95391824d6e5fc714c7cbdb55700
Sean <sean.t.irby@gmail.com>
2014-03-23 01:08:40 -0700

    Add key elements to fluid style

--- js/script.js
+++ js/script.js
@@ -1,22 +1,42 @@
 htmlKeyboard = (function(JSHelpers){
-  var scale = 60; // length of 1 key in pixels
+
+  function handleArrowKeys(keyElem, key){
+    keyElem.style.height = "50%";;
+    if(key.name == "up"){
+      keyElem.style.top = "0";
+    }
+    else{
+      keyElem.style.top = "50%";
+    }
+  }
 
   return {
     init: function(keyboard){
-      var container = document.getElementById("keyboard-container");
-      var key;
-      keyboard.forEach(function(elem, i){
-        elem.forEach(function(elem, j){
-          key = JSHelpers.createNode("<button class='key'></button>");
-          container.appendChild(key);
-          key.style.width = scale * keyboard[i][j].width+"px";
-          key.style.height = scale * keyboard[i][j].height+"px";
-          key.style.left = scale * keyboard[i][j].x + "px";
-          key.style.top = scale * keyboard[i][j].y + "px";
-          if( key.style.borderRadius == "" ){
-            key.style.borderRadius = 1/8*scale + "px";
+      var containerElem = document.getElementById("keyboard-container"),
+          keyboardElem = JSHelpers.createNode("<div id='keyboard'></div>"),
+          keyElem,
+          rowElem;
+
+      containerElem.appendChild(keyboardElem);
+      containerElem.style.paddingBottom = keyboard.height*100 + "%";
+
+      keyboard.keys.forEach(function(key_row, i){
+
+        rowElem = JSHelpers.createNode("<div class='key-row'></div>");
+        keyboardElem.appendChild(rowElem);
+        rowElem.style.height = (key_row[0].height/keyboard.height)*100 + "%";
+        rowElem.style.top = (key_row[0].y/keyboard.height)*100 + "%";
+
+        key_row.forEach(function(key, j){
+          keyElem = JSHelpers.createNode("<button class='key'></button>");
+          rowElem.appendChild(keyElem);
+          keyElem.style.left = key.x*100 + "%";
+          keyElem.style.width = key.width*100 + "%";
+          JSHelpers.setText(keyElem, key.name);
+          if(key.name == "up" || key.name == "down" || key.name == "left" || key.name == "right"){
+            handleArrowKeys(keyElem, key);
           }
-        });
+        })
       });
     }
   }
