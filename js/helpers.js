432f476c06f17ba99d5a11366bd916e5173927e0
Sean <sean.t.irby@gmail.com>
2014-03-30 20:28:01 -0700

    Define JSHelpers as a property of the global object

--- js/helpers.js
+++ js/helpers.js
@@ -1,103 +1,101 @@
-var JSHelpers = (function(){
-  return {
+this.JSHelpers = {
 
-    // Cross browser on document ready function
-    // Accepts function to call, context, and arguments
-    ready: function(func, context, arguments){
-      if (document.addEventListener){
-        document.addEventListener('DOMContentLoaded', func.apply(context, arguments));
-      } else {
-        document.attachEvent('onreadystatechange', function(){
-          if(document.readyState === 'interactive')
-            func.apply(context, arguments)
-        })
-      }
-    },
+  // Cross browser on document ready function
+  // Accepts function to call, context, and arguments
+  ready: function(func, context, arguments){
+    if (document.addEventListener){
+      document.addEventListener('DOMContentLoaded', func.apply(context, arguments));
+    } else {
+      document.attachEvent('onreadystatechange', function(){
+        if(document.readyState === 'interactive')
+          func.apply(context, arguments)
+      })
+    }
+  },
 
 
-    addEventListener: function(el, eventName, handler){
-      if (el.addEventListener) {
-        el.addEventListener(eventName, handler);
-      } else {
-        el.attachEvent('on' + eventName, function(){
-          handler.call(el);
-        });
-      }
-    },
-
-    simulateMouseEvent: function(el, type){
-      if (document.createEvent){
-        var event = new MouseEvent(type, {
-          'view': window,
-          'bubbles': false,
-          'cancelable': true
-        });
-      el.dispatchEvent(event);
-      } else {
-        el.fireEvent('on'+type);
-      }
-    },
-
-    addClass: function(el, clazz){
-      if(el.classList){
-        el.classList.add(clazz)
-      } else {
-        el.className += ' ' + clazz;
-      }
-    },
-
-    removeClass: function(el, clazz){
-      if (el.classList){
-        el.classList.remove(clazz);
-      } else {
-        el.className = el.className.replace(new RegExp('(^|\\b)' + clazz.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
-      }
-    },
-
-    // NOTE: This function doesn't support all node types yet ('th', 'tr', etc..)
-    // TODO: Fix above;
-    createNode: function(string){
-      var div = document.createElement('div');
-      div.innerHTML = string;
-      var elements = div.childNodes;
-      return elements[0];
-    },
-
-    setText: function(el, string){
-      if (el.textContent !== undefined)
-        el.textContent = string;
-      else
-        el.innerText = string;
-    },
-
-    createTable: function(headings){
-      var acc = "";
-      headings.forEach(function(elem, i){
-        acc = acc + "<th>" + headings[i] + "</th>";
+  addEventListener: function(el, eventName, handler){
+    if (el.addEventListener) {
+      el.addEventListener(eventName, handler);
+    } else {
+      el.attachEvent('on' + eventName, function(){
+        handler.call(el);
       });
-      return this.createNode("<table><thead>" + acc + "</thead><tbody></tbody></table>")
-    },
+    }
+  },
 
-    indexOf: function(needle) {
-      if(typeof Array.prototype.indexOf === 'function') {
-          indexOf = Array.prototype.indexOf;
-      } else {
-          indexOf = function(needle) {
-            var i = -1, index = -1;
-            for(i = 0; i < this.length; i++) {
-                if(this[i] === needle) {
-                  index = i;
-                  break;
-                }
-            }
-            return index;
+  simulateMouseEvent: function(el, type){
+    if (document.createEvent){
+      var event = new MouseEvent(type, {
+        'view': window,
+        'bubbles': false,
+        'cancelable': true
+      });
+    el.dispatchEvent(event);
+    } else {
+      el.fireEvent('on'+type);
+    }
+  },
+
+  addClass: function(el, clazz){
+    if(el.classList){
+      el.classList.add(clazz)
+    } else {
+      el.className += ' ' + clazz;
+    }
+  },
+
+  removeClass: function(el, clazz){
+    if (el.classList){
+      el.classList.remove(clazz);
+    } else {
+      el.className = el.className.replace(new RegExp('(^|\\b)' + clazz.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+    }
+  },
+
+  // NOTE: This function doesn't support all node types yet ('th', 'tr', etc..)
+  // TODO: Fix above;
+  createNode: function(string){
+    var div = document.createElement('div');
+    div.innerHTML = string;
+    var elements = div.childNodes;
+    return elements[0];
+  },
+
+  setText: function(el, string){
+    if (el.textContent !== undefined)
+      el.textContent = string;
+    else
+      el.innerText = string;
+  },
+
+  createTable: function(headings){
+    var acc = "";
+    headings.forEach(function(elem, i){
+      acc = acc + "<th>" + headings[i] + "</th>";
+    });
+    return this.createNode("<table><thead>" + acc + "</thead><tbody></tbody></table>")
+  },
+
+  indexOf: function(needle) {
+    if(typeof Array.prototype.indexOf === 'function') {
+        indexOf = Array.prototype.indexOf;
+    } else {
+        indexOf = function(needle) {
+          var i = -1, index = -1;
+          for(i = 0; i < this.length; i++) {
+              if(this[i] === needle) {
+                index = i;
+                break;
+              }
           }
-      }
-      return indexOf.call(this, needle);
-    },
-    // TODO: Create a cross browser 'forEach' function that maintains context, replace all instances in project
-    forEach: function(arr, func){},
-    // TODO: Create a cross browser 'map' function
-    map: function(arr, func){}
-  }
-})()
+          return index;
+        }
+    }
+    return indexOf.call(this, needle);
+  },
+  // TODO: Create a cross browser 'forEach' function that maintains context, replace all instances in project
+  forEach: function(arr, func){},
+  // TODO: Create a cross browser 'map' function
+  map: function(arr, func){}
+}
