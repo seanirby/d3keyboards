00d1f5c1b624057ccde9c6c25188151c3525c3bd
Sean <sean.t.irby@gmail.com>
2014-04-04 00:16:47 -0700

    Take server file out of version control

--- server.rb
+++
@@ -1,9 +0,0 @@
-require 'webrick'
-
-root = File.expand_path ""
-puts "Starting server with root: #{root}"
-server = WEBrick::HTTPServer.new :Port => 8000, :DocumentRoot => root
-
-trap 'INT' do server.shutdown end
-
-server.start
